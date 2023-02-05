8e9f5bd04f997c8cc71b293325c71caa326aaeaf
Tatiana Grigorieva <tatiana.grigorieva@softwarecountry.com>
2023-02-05 17:04:24 +0100

    Started to create test for item service

---
+++ Tests/Database Tests/MixERP.Net.Tests.PgUnitTest.EF/IntegrationTests/CoreTests/ItemsServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using MixERP.Net.DatabaseLayer.Core;
+using MixERP.Net.DBFactory.EF.Context;
+using Moq;
+using NUnit.Framework;
+
+namespace MixERP.Net.Tests.PgTest.EF.IntegrationTests.CoreTests
+{
+    [TestFixture]
+    public class ItemsServiceTest
+    {
+        private ItemsService _itemsService;
+        private MixerpContext _mixerpContextMock;
+        private Mock<IProcedureExecutor> _procedureExecutorMock;
+        [SetUp]
+        public void Setup()
+        {
+            _mixerpContextMock = new MixerpContext();
+            _procedureExecutorMock = new Mock<IProcedureExecutor>();
+            _itemsService = new ItemsService(_mixerpContextMock, _procedureExecutorMock.Object);
+        }
+
+        [Test]
+        public void ItemExistsByCode_NotExistingCodereturnFalse()
+        {
+            string itemCode = "1";
+            var result = _itemsService.ItemExistsByCode(itemCode);
+            Assert.IsFalse(result);
+        }
+    }
+}
